6d92e5f54975d7ba11bb689d1fb1f45b07edd6f2
hafsafathy <hafsafathy020@gmail.com>
2024-02-13 14:36:15 +0000

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """If not a valid subreddit, return 0."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
+    headers = {'User-Agent': 'Custom-User-Agent'}
     response = get(url, headers=headers)
     reddits = response.json()
 
