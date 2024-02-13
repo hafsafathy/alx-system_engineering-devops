65d5a6044ea62bcdc3a559349b34aef47678ec5f
hafsafathy <hafsafathy020@gmail.com>
2024-02-13 14:39:55 +0000

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,22 @@
 """
 
 from json import loads
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """If not a valid subreddit, return 0."""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Custom-User-Agent'}
-    response = get(url, headers=headers)
-    reddits = response.json()
-
-    try:
-        subscribers = reddits.get('data').get('subscribers')
-        return int(subscribers)
-    except:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        # Extract the number of subscribers from the JSON response
+        data = response.json().get('data', {})
+        subscribers = data.get('subscribers', 0)
+        return subscribers
+    else:
+        # Return 0 for invalid subreddits or other errors
         return 0
+
