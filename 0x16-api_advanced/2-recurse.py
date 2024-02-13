a311a3a7bc7d339310731014aa3b7713d15cd11d
hafsafathy <hafsafathy020@gmail.com>
2024-02-13 15:21:33 +0000

    for fix errors

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,29 +4,38 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=None, after=None):
+    """Recursively retrieves the titles of all hot articles for a given subreddit."""
+    if hot_list is None:
+        hot_list = []
+
+    if subreddit is None or not isinstance(subreddit, str):
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = { "User-Agent": "0x16-api_advanced:project:\
+                 v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {'limit': 100}
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
+    if after:
+        params['after'] = after
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+
+        for child in children:
+            post = child.get('data', {})
+            title = post.get('title')
+            hot_list.append(title)
+
+        after = data.get('after')
+        if after:
+            recurse(subreddit, hot_list, after)
+
+    elif response.status_code == 404:
+        return None
     return hot_list
